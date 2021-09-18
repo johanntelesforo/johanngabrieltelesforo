450a33b176485e64eece37ad5a1b47fbfd5cfa06
Johann Telesforo <jtelesforo_2000000000102@uic.edu.ph>
2021-09-18 22:10:15 +0800

    created contact webpage

---
+++ contact.js
@@ -0,0 +1,8 @@
+let name = document.querySelector('#name');
+let email = document.querySelector('#email');
+let sendbtn = document.querySelector('.sendbtn');
+
+function clickFunction(){
+    alert('Message has been sent to Johann!');
+    location.reload();
+}
